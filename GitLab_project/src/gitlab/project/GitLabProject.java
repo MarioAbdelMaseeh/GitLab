84612e2d730b6c95c214ac18710a0c1e6af4ecba
Mario <90201555+MarioAbdelMaseeh@users.noreply.github.com>
2024-12-31 15:06:32 +0200

    added comments

--- GitLab_project/src/gitlab/project/GitLabProject.java
+++ GitLab_project/src/gitlab/project/GitLabProject.java
@@ -16,6 +16,13 @@ public class GitLabProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ///
+        //
+        //
+        //
+        //
+        //
+        //
     }
     
 }
